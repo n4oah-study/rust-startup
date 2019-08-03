576d19f5360891f86bb530da85a75dab6797e1b3
n4oah <n4oahdev@gmail.com>
2019-08-03 21:10:30 +0900

    data_type commit

---
+++ basic01/data_type/target/debug/deps/data_type-73f03f0680a75159.d
@@ -0,0 +1,5 @@
+/mnt/g/workspace/git/rust-startup/basic01/data_type/target/debug/deps/data_type-73f03f0680a75159: src/main.rs
+
+/mnt/g/workspace/git/rust-startup/basic01/data_type/target/debug/deps/data_type-73f03f0680a75159.d: src/main.rs
+
+src/main.rs:
