f07ed1b05714e3ca0bcb13a92b3865021feba901
n4oah <n4oahdev@gmail.com>
2019-08-04 11:08:10 +0900

    중간 커밋

---
+++ basic01/function/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    hello_function(12, 45);
+}
+
+fn hello_function(a: i32, b: i32) {
+    println!("a: {}, b: {}", a, b);
+}
---
+++ basic01/function/target/debug/function.d
@@ -0,0 +1,1 @@
+/mnt/g/workspace/git/rust-startup/basic01/function/target/debug/function: /mnt/g/workspace/git/rust-startup/basic01/function/src/main.rs
