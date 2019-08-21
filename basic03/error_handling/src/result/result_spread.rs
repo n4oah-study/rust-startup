717c4f16b22c1b966722dd28d7c7c35c06a0ce48
n4oah <n4oahdev@gmail.com>
2019-08-21 22:29:26 +0900

    에러 전파

--- basic03/error_handling/src/result/result_spread.rs
+++ basic03/error_handling/src/result/result_spread.rs
@@ -1,15 +1,26 @@
 /*
 에러 전파하기
 */
+use std::io;
+use std::fs::File;
+use std::io::Read;
+
 pub fn result_spread_func() {
-    
+    let res = error_spread_func();
+    println!("{:?}", res);
 }
 
 pub fn error_spread_func() -> Result<String, io::Error> {
-    let f = File::open("hello3.txt")
+    let f = File::open("hello3.txt");
 
-    let f = match f {
+    let mut f = match f {
         Ok(file) => file,
-        Err(error) => Err(e)
+        Err(error) => return Err(error) // 에러시 error_spread_func을 리턴 함
     };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(error) => Err(error)
+    }
 }
