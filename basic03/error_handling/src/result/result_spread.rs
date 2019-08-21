cec6e716c1b628808ee63aa2d118a2298fd7fa94
n4oah <n4oahdev@gmail.com>
2019-08-21 22:44:53 +0900

    에러 전파

--- basic03/error_handling/src/result/result_spread.rs
+++ basic03/error_handling/src/result/result_spread.rs
@@ -6,11 +6,16 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn result_spread_func() {
+    println!("========== error_spread_func ===========");
     let res = error_spread_func();
-    println!("{:?}", res);
+    println!("error_spread_func return: {:?}", res);
+
+    println!("========== error_spread_short_func ===========");
+    let res = error_spread_short_func();
+    println!("error_spread_short_func return: {:?}", res);
 }
 
-pub fn error_spread_func() -> Result<String, io::Error> {
+fn error_spread_func() -> Result<String, io::Error> {
     let f = File::open("hello3.txt");
 
     let mut f = match f {
@@ -23,4 +28,14 @@ pub fn error_spread_func() -> Result<String, io::Error> {
         Ok(_) => Ok(s),
         Err(error) => Err(error)
     }
+}
+
+// ? 키워드로 Result가 Error일시 호출한쪽으로 에러를 전파함
+fn error_spread_short_func() -> Result<String, io::Error> {
+    let mut f = File::open("hello3.txt")?;
+
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    
+    Ok(s)
 }
